a748372abe907ab633c0ab694a5d051b3891e5be
Adrian Gaborek <A.Gaborek@devnet.de>
2024-03-18 15:11:05 +0100

    add certificate pages

---
+++ Client/Data/Components/StoryblokCertificate.cs
@@ -0,0 +1,13 @@
+﻿using Storyblok;
+using Storyblok.Attributes;
+using System.Text.Json.Serialization;
+
+namespace Client.Data.Components;
+
+[StoryblokComponent("certificate")]
+public class StoryblokCertificate : StoryblokComponent
+{
+    [JsonPropertyName("image")]
+    public StoryblokAsset? Image { get; set; }
+
+}
